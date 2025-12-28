7d93406d06ca1d3c3ff4bd1e7a398f9b0048642e
Goga <goga.kirvalidze32b@geolab.edu.ge>
2025-12-28 18:55:05 +0400

    fix supabase client

--- vinme-clone/src/lib/supabase.ts
+++ vinme-clone/src/lib/supabase.ts
@@ -1,13 +1,10 @@
+// src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL=https://kcbblvccsbvsbhofxnmm.supabase.co!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY=sb_publishable_HPCE7UchjfZ4_OpU9daNNA_okhL8TDO!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
 );
-
-
-
-
