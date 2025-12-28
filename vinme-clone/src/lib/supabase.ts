6099d6b2bef5418d2a3b0114c74017ccc8b7a315
Goga <goga.kirvalidze32b@geolab.edu.ge>
2025-12-28 19:10:13 +0400

    fix supabase client

--- vinme-clone/src/lib/supabase.ts
+++ vinme-clone/src/lib/supabase.ts
@@ -1,10 +1,6 @@
-// src/lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
