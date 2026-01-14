bbb7b4fc08e6e0267836dec3dc61412830ff66d0
Goga <goga.kirvalidze32b@geolab.edu.ge>
2026-01-14 17:23:41 +0400

    fix dynamic import syntax for turbopack

--- vinme-clone/src/app/chat/page.tsx
+++ vinme-clone/src/app/chat/page.tsx
@@ -1,13 +1,9 @@
 import dynamic from "next/dynamic";
 
-const MessagesClient = dynamic(() => import("./MessagesClient"), {
-  ssr: false,
-  loading: () => (
-    <div className="min-h-[100dvh] bg-black text-white flex items-center justify-center">
-      Loading…
-    </div>
-  ),
-});
+const MessagesClient = dynamic(
+  () => import("./MessagesClient"),
+  { ssr: false }
+);
 
 export default function Page() {
   return <MessagesClient />;
