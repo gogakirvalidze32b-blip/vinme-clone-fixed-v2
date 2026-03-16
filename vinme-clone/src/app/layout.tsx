e7212c3088df18b2b1776bde0fb686ddc06e38b7
Goga <goga.kirvalidze32b@geolab.edu.ge>
2026-03-16 19:51:08 +0400

    added manifest.json

--- vinme-clone/src/app/layout.tsx
+++ vinme-clone/src/app/layout.tsx
@@ -11,13 +11,6 @@ const notoGeo = Noto_Sans_Georgian({
   variable: "--font-brand",
 });
 
-// 2. მეტამონაცემები (მანიფესტი აქ ჩაიწერა, ცალკე ლინკი აღარ გინდა)
-export const metadata: Metadata = {
-  title: "შეხვდი",
-  description: "Dating app",
-  icons: { icon: "/favicon.ico" },
-  manifest: "/manifest.json",
-};
 
 // 3. ვიუპორტის კონფიგურაცია
 export const viewport: Viewport = {
