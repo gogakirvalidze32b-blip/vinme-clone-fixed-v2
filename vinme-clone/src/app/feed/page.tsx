de8cc3c13e9d0a7a1fbf7358db5bc823ebb7b9ac
Goga <goga.kirvalidze32b@geolab.edu.ge>
2026-03-16 21:43:24 +0400

    feed fixed

--- vinme-clone/src/app/feed/page.tsx
+++ vinme-clone/src/app/feed/page.tsx
@@ -1,4 +1,4 @@
-
+"use client";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import TinderCard from "@/components/TinderCard";
